9b5ee9ca7280d19d985693772874bd94e8397c74
karthik.c <bhanukarthik2002@gmail.com>
2015-07-06 18:31:08 -0700

    changed name of resta to Restaurant

--- Restaurant.java
+++ Restaurant.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Resta {
+public class Restaurant {
 
     public static void main(String[] args) {
 		// TODO Auto-generated method stub
